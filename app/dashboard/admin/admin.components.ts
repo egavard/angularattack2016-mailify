b84a7687bfea80aa65417f1338f7477f9a8972ff
egavard <ewann.gavard@sarbacane.com>
2016-05-14 19:13:24 +0200

    admin drag n drop

--- app/dashboard/admin/admin.components.ts
+++ app/dashboard/admin/admin.components.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {ModulesService} from "../../services/modules.service";
 import {ModuleMetadata} from "../../modules/module-metadata.model";
 import {DebugModule} from "../../modules/debug-module.component";
+import {GridsterItem} from '../../libs/gridster/gridster';
+declare var $;
 
 @Component({
     selector: 'home',
@@ -10,8 +12,30 @@ import {DebugModule} from "../../modules/debug-module.component";
 })
 export class AdminComponent {
     private availableModules: ModuleMetadata[];
+    private _items:GridsterItem[];
 
     constructor(private modulesService: ModulesService) {
         this.availableModules = modulesService.getAvailableModules();
+        $('.gridster').gridster({
+            max_cols:12,
+            draggable:{
+                stop:function(event,ui){
+                    console.log('stop');
+                    console.log(event);
+                },
+                start:function(event,ui){
+
+                }
+            }
+        })
+    }
+
+
+    get items():GridsterItem[] {
+        return this._items;
+    }
+
+    set items(value:Array) {
+        this._items = value;
     }
 }
