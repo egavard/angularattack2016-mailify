f24408e7993c4ad1f6eaff629a58d20ae18c7de8
rwinckel <remi.winckel@sarbacane.com>
2016-05-14 10:59:24 +0200

    Add Display line ! :)

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,13 @@ import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import {Gridster} from './libs/gridster/gridster.component';
 import {DebugModule} from './modules/debug-module.component';
 import {DataProviderService} from './services/data-provider.service';
+import {CHART_DIRECTIVES} from './libs/ng2-charts-upgrade-rc1/ng2-charts';
 
 @Component({
     selector:'app',
     moduleId:module.id,
     templateUrl:'./app.html',
-    directives:[Gridster]
+    directives:[Gridster, CHART_DIRECTIVES]
 })
 export class AppComponent implements AfterViewInit{
     @ViewChild(Gridster) gridster:Gridster;
@@ -26,5 +27,62 @@ export class AppComponent implements AfterViewInit{
     constructor(private dataProviderService:DataProviderService){
         console.log(dataProviderService.getBasicChart());
         
-    } 
+    }
+
+
+
+
+// lineChart
+    private lineChartData:Array<any> = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90],
+        [18, 48, 77, 9, 100, 27, 40]
+    ];
+    private lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    private lineChartSeries:Array<any> = ['Series A', 'Series B', 'Series C'];
+    private lineChartOptions:any = {
+        animation: false,
+        responsive: true,
+        multiTooltipTemplate: '<%if (datasetLabel){%><%=datasetLabel %>: <%}%><%= value %>'
+    };
+    private lineChartColours:Array<any> = [
+        { // grey
+            fillColor: 'rgba(148,159,177,0.2)',
+            strokeColor: 'rgba(148,159,177,1)',
+            pointColor: 'rgba(148,159,177,1)',
+            pointStrokeColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(148,159,177,0.8)'
+        },
+        { // dark grey
+            fillColor: 'rgba(77,83,96,0.2)',
+            strokeColor: 'rgba(77,83,96,1)',
+            pointColor: 'rgba(77,83,96,1)',
+            pointStrokeColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(77,83,96,1)'
+        },
+        { // grey
+            fillColor: 'rgba(148,159,177,0.2)',
+            strokeColor: 'rgba(148,159,177,1)',
+            pointColor: 'rgba(148,159,177,1)',
+            pointStrokeColor: '#fff',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(148,159,177,0.8)'
+        }
+    ];
+    private lineChartLegend:boolean = true;
+    private lineChartType:string = 'radar';
+
+    // events
+    chartClicked(e:any) {
+        console.log(e);
+    }
+
+    chartHovered(e:any) {
+        console.log(e);
+    }
+
+
+
 }
