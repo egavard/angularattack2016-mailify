9ebfb88eb038b91c76ae544be299781a8f1eb7d3
egavard <ewann.gavard@sarbacane.com>
2016-05-14 03:50:06 +0200

    Replaced main && app to use typescript.

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by egavard on 14/05/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'app',
+    moduleId:module.id,
+    templateUrl:'./app.html'
+})
+export class AppComponent{
+
+}
--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
+"use strict";
+/**
+ * Created by egavard on 14/05/16.
+ */
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=main.js.map
