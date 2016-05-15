034a1cf105b6d817679194e63be28905e984f53c
egavard <ewann.gavard@sarbacane.com>
2016-05-15 20:19:45 +0200

    module loading

--- app/dashboard/admin/admin.components.ts
+++ app/dashboard/admin/admin.components.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {ModulesService} from '../../services/modules.service';
 import {ModuleMetadata} from '../../modules/module-metadata.model';
 import {DebugModule} from '../../modules/debug-module.component';
-import {AfterViewInit} from '@angular/core'
+import {log} from '../../decorators/log.decorator';
+import { AfterViewInit } from '@angular/core'
 import {ChartModule} from '../../modules/chart-module.component';
 import {DataProviderService} from '../../services/data-provider.service';
 import {ChartPositionInformation} from '../../modules/chart-position-information';
 import {NgGrid, NgGridItem} from 'angular2-grid/dist/NgGrid';
-import {ChartType} from '../../modules/chart-module-metadata.model';
-import {ModuleConfigService} from "../../services/module-config.service";
+import {Type} from '@angular/core';
+import {ChartModuleMetadata, ChartType} from '../../modules/chart-module-metadata.model';
+
 declare var $;
 
 @Component({
@@ -20,18 +22,20 @@ export class AdminComponent implements AfterViewInit {
     private availableModules: ModuleMetadata[];
     private _items:ChartModule[];
 
-    constructor(private modulesService:ModulesService,
-                private dataProviderService:DataProviderService,
-                private moduleConfigService:ModuleConfigService) {
-        this._availableModules = modulesService.getAvailableModules();
+    constructor(private modulesService: ModulesService, private dataProviderService:DataProviderService) {
+        this.availableModules = modulesService.getAvailableModules();
+        let storedItems = window.localStorage.getItem('charts');
+        if(storedItems){
+            this.items = storedItems;
+        }else{
+            let item1 = new ChartModule(dataProviderService, 'ChartModule');
+            item1.chartPositionInformation = new ChartPositionInformation(0,0,5,1);
+            let item2 = new ChartModule(dataProviderService, 'HealthModule');
+            item2.chartPositionInformation = new ChartPositionInformation(6,0,5,1);
 
-        let item1 = new ChartModule(dataProviderService);
-        item1.chartPositionInformation = new ChartPositionInformation(0, 0, 5, 1);
-        let item2 = new ChartModule(dataProviderService);
-        item2.chartPositionInformation = new ChartPositionInformation(6, 0, 5, 1);
-
-        this._items = [];
-        this._items.push(item1, item2);
+            this.items = [];
+            this.items.push(item1, item2);
+        }
     }
 
     ngAfterViewInit(){
